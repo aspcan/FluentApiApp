5cc7017fa47a85241d9c65cca6a406ff8180d2aa
Yusuf Tatlı <yusuf.tatli@cottgroup.com>
2019-02-20 12:33:10 +0300

    Initial commit

---
+++ FluentApiApp/FluentApiApp/Validator/RegistrationViewModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentApiApp.Models;
+using FluentValidation;
+
+namespace FluentApiApp.Validator
+{
+    public class RegistrationViewModelValidator : AbstractValidator<RegistrationViewModel>
+    {
+        public RegistrationViewModelValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name boş geçilemez");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("LAst name boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("email boş geçilemez");
+        }
+    }
+}
